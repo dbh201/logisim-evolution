86a9af8f60d2b06c0fad085a342c2085f02fc378
delicious_spaghetti <47301809+dbh201@users.noreply.github.com>
2022-01-18 11:15:57 -0400

    bugfix: filter returning empty set

--- build.gradle.kts
+++ build.gradle.kts
@@ -710,10 +710,14 @@ tasks {
 
     // let's use google_checks.xml config provided with Checkstyle.
     // https://stackoverflow.com/a/67513272/1235698
-    val archive = configurations.checkstyle.get().resolve().filter {
-      name.startsWith("checkstyle")
+    val archive_list = configurations.checkstyle.get().resolve()
+    for(a in archive_list) {
+      if(a.name.startsWith("checkstyle")) {
+        config = resources.text.fromArchiveEntry(a, "google_checks.xml")
+        break
+      }
     }
-    config = resources.text.fromArchiveEntry(archive, "google_checks.xml")
+    
 
     // FIXME: There should be cleaner way of using custom suppression config with built-in style.
     // https://stackoverflow.com/a/64703619/1235698
